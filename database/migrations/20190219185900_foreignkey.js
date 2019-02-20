f6c92d5c93449a7489d76e33754ae6f15ea8fbd5
kevin tauzer <kevint@kevins-MBP.home>
2019-02-20 11:14:21 -1000

    add migrations

---
+++ database/migrations/20190219185900_foreignkey.js
@@ -0,0 +1,9 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.table('users', table => {
+    table.integer('contacts_id').references('contacts_id').inTable('contacts').notNullable();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
