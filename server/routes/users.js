261a41b4bf7dca334f90b193fe44ea1e3e408129
kevin tauzer <kevint@kevins-MBP.home>
2019-02-20 16:29:32 -1000

    add get route to users

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const contacts = require('./contacts');
+const 
+const app =express();
 
-router.get('/', (req, res) => {
-  res.send('These should be users');
+router.route('/')
+.get( (req,res) => {
+  console.log('hitting')
+  User.fetchAll()
+    .then(users => {
+      console.log(users)
+      res.json(users);
+    })
+    .catch(() => {
+      res.status(500).send('error finding users');
+    });
 });
 
-app.use('/contacts', contacts)
+
+app.use('/contacts', contacts);
 module.exports = router;
